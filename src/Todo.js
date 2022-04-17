be770d57099da29ba25626b5eb3c15df18ff3d9a
Taneya <twil609@aucklanduni.ac.nz>
2022-04-17 11:53:46 +1000

    remove comments

--- src/Todo.js
+++ src/Todo.js
@@ -2,16 +2,12 @@ import React from 'react'
 
 export default function Todo( { todo, toggleTodo } ) {
 
-    // create function for onChange because need to pass through id
     function handleTodoClick() {
         toggleTodo(todo.id)
     }
 
   return (
     <div>
-        {/* to print todos */}
-        {/* include name reference from App.js */}
-
         
         <label>
             <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
